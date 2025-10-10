058419ff77257c599fbc8747cc5c1eaddaf14931
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-10-10 13:06:24 +0300

    add middleware

--- src/server.js
+++ src/server.js
@@ -2,21 +2,28 @@ import 'dotenv/config';
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
 });
 
 app.get('/notes/:noteId', (req, res) => {
-  res.status(200).json({ message: 'Retrieved note with ID: id_param' });
+  const noteId = req.params.noteId;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
+// Маршрут для тестування middleware помилки
 app.get('/test-error', (req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-// Запуск сервера
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Middleware 404 (після всіх маршрутів)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
 });
