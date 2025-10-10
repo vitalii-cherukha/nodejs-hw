da3009dce6a036670a9a28d58f84ecc7f46af6f1
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-10-10 13:25:04 +0300

    add cors/express/pino-http

--- src/server.js
+++ src/server.js
@@ -1,13 +1,29 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-// Запуск сервера
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
@@ -19,11 +35,25 @@ app.get('/notes/:noteId', (req, res) => {
 });
 
 // Маршрут для тестування middleware помилки
-app.get('/test-error', (req, res) => {
-  res.status(404).json({ message: 'Route not found' });
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
 });
 
 // Middleware 404 (після всіх маршрутів)
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
+});
+
+// Middleware для обробки помилок (останнє)
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: 'Internal Server Error',
+    error: err.message,
+  });
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
