b59dabf7722cac75e403f09845a7ffe7e85a8fba
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-10-17 17:30:18 +0300

    add connectMongoDB

--- src/server.js
+++ src/server.js
@@ -1,1 +1,69 @@
-//test
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Notes API is running',
+  });
+});
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const noteId = req.params.noteId;
+  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
+});
+
+// Маршрут для тестування middleware помилки
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+// Middleware 404 (після всіх маршрутів)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Middleware для обробки помилок (останнє)
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
+  });
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
